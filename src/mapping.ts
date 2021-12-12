ea472ad579b319feb79f12777b791a518ac9114b
victorjambo <victorjambo@live.com>
2021-12-12 22:07:49 +0300

    something something

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,6 +5,7 @@ export function handleMinted(event: Minted): void {
   let token = Token.load(event.params.tokenId.toString())
   if (!token) {
     let token = new Token(event.params.tokenId.toString());
+    token.tokenId = event.params.tokenId;
     token.owner = event.params.owner;
     token._tokenName = event.params._tokenName;
     token._price = event.params._price;
